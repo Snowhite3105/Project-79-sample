ee4f8e32268dc6a9fec3d9808b9a096915879cc1
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2021-07-29 18:13:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+student_array=[];
+function submit() {
+    var Student_1=document.getElementById("name_of_the_student_1").value;
+     var Student_2=document.getElementById("name_of_the_student_2").value;
+    var Student_3=document.getElementById("name_of_the_student_3").value;
+    var Student_4=document.getElementById("name_of_the_student_4").value;
+    var Student_5=document.getElementById("name_of_the_student_5").value;
+    student_array.push(Student_1);
+    student_array.push(Student_2);
+   student_array.push(Student_3);
+    student_array.push(Student_4);
+    student_array.push(Student_5);
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
+    document.getElementById("submit_button").style.display="none";
+    document.getElementById("sort_button").style.display="inline-block";
+}
+function sorting(){
+    student_array.sort();
+    console.log(student_array);
+    document.getElementById("display_name").innerHTML=student_array;
+}
